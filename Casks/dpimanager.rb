1cbfffc951b591c28bd23551bca4f509e61f76b2
Harsh <131232263+0xDranzer@users.noreply.github.com>
2026-05-30 16:59:31 +0530

    Create dpimanager.rb

---
+++ Casks/dpimanager.rb
@@ -0,0 +1,15 @@
+cask "dpimanager" do
+  version "1.0.0"
+  sha256 "eafd8f5178000595c96a52a938a08cf853022140f651820488cd422bdaa9482f"
+
+  url "https://github.com/0xDranzer/MAC_DPIManager/releases/download/v#{version}/DPIManager.zip"
+  name "DPIManager"
+  desc "Enable HiDPI modes and adjust font smoothing for external displays"
+  homepage "https://github.com/0xDranzer/MAC_DPIManager"
+
+  app "DPIManager.app"
+
+  zap trash: [
+    "/Library/Displays/Contents/Resources/Overrides",
+  ]
+end
